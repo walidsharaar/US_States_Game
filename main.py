800c5bc2a959ec6fa48acecee561f1fb6eaf31a0
Sharaar <walidsharar@gmail.com>
2021-06-27 19:06:34 +0200

    Final Refinements

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import pandas
 
 
 screen = turtle.Screen()
-screen.title("States Game")
+screen.title("U.S. States Game")
 image= "states.gif"
 screen.addshape(image)
 turtle.shape(image)
 
 
 data=pandas.read_csv("50_states.csv")
-states=data.state.to_list()
-print(states)
+all_states=data.state.to_list()
 guessed_states=[]
+
 while len(guessed_states)<50:
 
     #askk the user
     answer= screen.textinput(title=f" {len(guessed_states)} State Name ",prompt="Enter a state name: ").title()
-    print(answer)
-    if answer in states:
+    if answer == "Exit":
+        missing_states=[]
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        # Generate the CSV file and the wrong states names.
+        new_data.to_csv("States_to_memorize.csv")
+        break
+    if answer in all_states:
         guessed_states.append(answer)
         my_turtle = turtle.Turtle()
         my_turtle.hideturtle()
@@ -28,7 +36,7 @@ while len(guessed_states)<50:
         my_turtle.write(answer)
 
 
-screen.exitonclick()
+
 
 
 

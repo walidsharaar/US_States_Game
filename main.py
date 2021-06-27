4f83c73e7ce412012d0354f13af12ad25efe7469
Sharaar <walidsharar@gmail.com>
2021-06-27 18:52:29 +0200

    Check user input and place the pointer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 
 screen = turtle.Screen()
@@ -7,8 +8,29 @@ image= "states.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-#askk the user
-answer= screen.textinput(title=" State Name ",prompt="Enter a state name: ")
-print(answer)
+
+data=pandas.read_csv("50_states.csv")
+states=data.state.to_list()
+print(states)
+guessed_states=[]
+while len(guessed_states)<50:
+
+    #askk the user
+    answer= screen.textinput(title=f" {len(guessed_states)} State Name ",prompt="Enter a state name: ").title()
+    print(answer)
+    if answer in states:
+        guessed_states.append(answer)
+        my_turtle = turtle.Turtle()
+        my_turtle.hideturtle()
+        my_turtle.penup()
+        state_data= data[data.state == answer]
+        my_turtle.goto(int(state_data.x),int(state_data.y))
+        my_turtle.write(answer)
+
+
+screen.exitonclick()
+
+
+
 
 

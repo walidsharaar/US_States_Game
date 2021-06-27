5147af4ba9f6ae876c5f0fab423f1440270a788c
Sharaar <walidsharar@gmail.com>
2021-06-27 18:35:20 +0200

    ask user about the input

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ image= "states.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+#askk the user
+answer= screen.textinput(title=" State Name ",prompt="Enter a state name: ")
+print(answer)
 
 
-screen.exitonclick()
